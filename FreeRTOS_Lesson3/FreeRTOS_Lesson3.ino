9ce4e38f66dc9b5d0c15743fe955b1e3bc642c59
evaesteban <eva.p.estebanvelasco@ieee.org>
2022-05-19 19:04:10 -0400

    completed basic code for lesson 3

---
+++ FreeRTOS_Lesson3/FreeRTOS_Lesson3.ino
@@ -0,0 +1,16 @@
+/** 
+ *  Digi-Key FreeRTOS Course Lesson 3
+ *  
+ *  Toggles LED and prints "Hello, World" to console in separate tasks
+ *  
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
