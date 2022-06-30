0e31f6dd97f2085fdd071bee4516e63b87387f4e
evaesteban <eva.p.estebanvelasco@ieee.org>
2022-06-30 17:32:50 -0400

    added lesson 7 folder

---
+++ FreeRTOS_Lesson7/FreeRTOS_Lesson7.ino
@@ -0,0 +1,16 @@
+/** 
+ *  Digi-Key FreeRTOS Course Lesson 7
+ *  
+ *  Increment a shared global variable.
+ *  
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
